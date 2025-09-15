965bdd6e31144af3f5176b89ac20bd69184785af
yishin9595 <yishin9595@gmail.com>
2025-09-16 00:07:30 +0800

    Create 04_crime_rate_vs_price_by_district_tpe_ntpc.sql

---
+++ sql_scripts/04_crime_rate_vs_price_by_district_tpe_ntpc.sql
@@ -0,0 +1,42 @@
+with real_estate_midian as (
+  select
+    county,
+    district,
+    approx_quantiles(unit_price, 2)[offset(1)] as median_unit_price
+  from `lisa-project-383407.2025_profile.taipei_newtaipei_real_estate_transactions_2020_202505`
+  group by county,district
+),
+
+crime_record_total as (
+  select
+    oc_county,
+    oc_region,
+    sum(total_cases) as total_crime_cases,
+  from `lisa-project-383407.2025_crime.crime_record_by_crime_rate_per_10k`
+  group by 1,2
+),
+
+populations_total as (
+  select
+    county,
+    district,
+    sum(populations) as total_populations,
+  from `lisa-project-383407.2025_profile.taiwan_population_2020_2024`
+  group by 1,2
+)
+
+
+select
+  c.oc_county,
+  c.oc_region,
+  r.median_unit_price,
+  c.total_crime_cases,
+  p.total_populations,
+  round(safe_divide(c.total_crime_cases , p.total_populations) * 10000 , 2) as crime_rate_per_10k
+from crime_record_total as c
+join real_estate_midian as r
+  on c.oc_county = r.county
+  and c.oc_region = r.district
+join populations_total as p
+  on r.county = p.county
+  and r.district = p.district;
