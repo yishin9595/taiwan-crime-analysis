080d3baa33e51804b6f4c13c195aa48d9345cbcc
yishin9595 <yishin9595@gmail.com>
2025-09-15 23:59:02 +0800

    Create 01_crime_rate_per_10k.sql

---
+++ sql_scripts/01_crime_rate_per_10k.sql
@@ -0,0 +1,35 @@
+with crime as 
+(
+  select
+    extract(year from oc_date) as year,
+    oc_county,
+    oc_region,
+    type,
+    count(type) as total_cases
+  from `lisa-project-383407.2025_profile.crime_statistics_2020_2024`
+  group by 1,2,3,4
+),
+
+populations as (
+  select 
+    year,
+    county,
+    district,
+    sum(populations) as populations
+  from `lisa-project-383407.2025_profile.taiwan_population_2020_2024`
+  group by 1,2,3
+)
+
+select 
+  p.year,
+  c.oc_county,
+  c.oc_region,
+  c.type,
+  c.total_cases,
+  p.populations,
+  round(safe_divide(total_cases,populations) *10000 ,2) as crime_rate_per_10k
+from crime as c
+join populations as p
+  on c.year = p.year
+  and c.oc_county = p.county
+  and c.oc_region = p.district
