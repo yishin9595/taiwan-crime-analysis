2d0ee287489c0cd026b4ded3023541917d182819
yishin9595 <yishin9595@gmail.com>
2025-09-16 00:02:17 +0800

    Create 02_crime_records_by_date.sql

---
+++ sql_scripts/02_crime_records_by_date.sql
@@ -0,0 +1,8 @@
+select
+  extract(year from oc_date) as year,
+  extract(month from oc_date) as month,
+  type,
+  oc_county,
+  oc_region
+from `lisa-project-383407.2025_profile.crime_statistics_2020_2024`
+order by year,month
